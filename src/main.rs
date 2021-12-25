ab121af6edfed060ca7e133996f8cb11d6077340
Isidro Vasquez <isivascor@gmail.com>
2021-12-25 13:59:36 -0600

    First commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+mod camera;
+
+pub use camera::*;
+struct Player;
+
+fn spawn_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+            sprite: Sprite::new(Vec2::new(1.0, 1.0)),
+            ..Default::default()
+        })
+        .insert(Player);
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(new_camera_2d());
+}
+
+
+
+
+fn main() {
+    App::build()
+        .insert_resource(WindowDescriptor {
+            title: "Platformer!".to_string(),
+            width: 640.0,
+            height: 400.0,
+            vsync: true,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_startup_system(setup.system())
+        .add_startup_stage("player_setup", SystemStage::single(spawn_player.system()))
+        .add_plugins(DefaultPlugins)
+        .run();
+}
